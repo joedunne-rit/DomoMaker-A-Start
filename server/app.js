045c39c68a6e4737911c3f3041a1d9ada20414c6
Joseph Dunne <jmd1494@rit.edu>
2023-03-29 12:41:19 -0400

    Part B completed mostly, need to check invalid data test

--- server/app.js
+++ server/app.js
@@ -6,17 +6,18 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
-mongoose.connect(dbURI).catch((err)=>{
-    if (err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+mongoose.connect(dbURI).catch((err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const app = express();
@@ -25,15 +26,21 @@ app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.engine('handlebars', expressHandlebars.engine({defaultLayout: ''}));
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 
 router(app);
 
-app.listen(port, (err)=>{
-    if (err) {throw err;}
-    console.log(`Listening on port ${port}`);
+app.listen(port, (err) => {
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
 });
