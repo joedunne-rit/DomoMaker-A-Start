3d967f4a7c9dcfbace6e63df7f386d66c9e78965
Joseph Dunne <jmd1494@rit.edu>
2023-03-29 11:01:19 -0400

    Part A finished

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({extende: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.engine('handlebars', expressHandlebars.engine({defaultLayout: ''}));
 app.set('view engine', 'handlebars');
